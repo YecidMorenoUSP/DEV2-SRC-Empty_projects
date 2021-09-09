dbe83f000cd23acf7b74f42e490908fed147b486
YecidMoreno <yecidmoreno@usp.br>
2021-09-09 14:20:22 -0300

    Update Rehamove for Raspberry

--- Py_Empty_Rehamove_FES/test.py
+++ Py_Empty_Rehamove_FES/test.py
@@ -5,18 +5,19 @@ import numpy as np
 r = Rehamove("COM13")
 
 while r.rehamove == None: 
-	r = Rehamove("COM14")
+	r = Rehamove("COM4")
 	time.sleep(0.5)
 
 t1 = time.time()
 
 tcur = 0
-maxmA = 69
-freqEst = 80
+maxmA = 20
+
+freqEst = 180
 freqD = (1/8)*2*np.pi;
 maVals = []
 tVals = []
-while(tcur < 8):
+while(tcur < 4):
         tcur = time.time()-t1
         valmA = np.sin(tcur*freqD) * maxmA
         if(abs(valmA)>maxmA):
